e50d063102e12e29577fb1a7ed0be737ba135ba7
Rudra143-lab <77666625+Rudra143-lab@users.noreply.github.com>
2021-07-05 09:55:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var audio = document.getElementById("audio");
+
+function h() {
+    window.location = "index.html";
+    audio.play();
+}
+
+function a() {
+    window.location = "about.html";
+    audio.play();
+}
+
+function ho() {
+    window.location = "hobbies.html";
+    audio.play();
+}
+
+function v() {
+    window.location = "video.html";
+    audio.play();
+}
+
+function c() {
+    window.location = "contact.html";
+    audio.play();
+}
